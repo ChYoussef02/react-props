628170a7da300bf7d94c85620c2461eb21cc179a
ChYoussef02 <youssef.chalbi669@gmail.com>
2021-08-29 15:52:25 +0100

    React Props

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Child from './profileComponent'
+
+const App = () => {
+ 
+  return (
+
+    <div style={{marginLeft:'14cm' , marginTop:'2.5cm'}}>
+    <Child fn='Chalbi Youssef' bio='I am a student in Gomycode Sousse ' profession='freelancing web-devolopment' Name='Youssef' ></Child>
+   
+   </div>
+    
+  );
+  
+}
+
+export default App;
